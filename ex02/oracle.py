5957525e5dfeb29a35fb7fd0abe1e04e5fe886a1
Matthieu Dourdoigne <mdourdoi@z4r4p2.42lyon.fr>
2026-02-27 17:10:29 +0100

    done

--- ex02/oracle.py
+++ ex02/oracle.py
@@ -1,21 +1,15 @@
 import os
-
-
-class ConfigError(Exception):
-    pass
+from typing import Dict, Any
 
 
 def get_variable(name: str) -> str | None:
-    value = os.gentenv(str(name))
-    if value is None:
-        raise ConfigError(f'{value} is missing in you environment')
-    return value
+    return os.environ[str(name)]
 
 
-def main() -> None:
+def get_env() -> Dict[str, Any] | None:
     from dotenv import load_dotenv
 
-    load_dotenv(override=False)
+    load_dotenv()
     variables_name = [
         "MATRIX_MODE",
         "DATABASE_URL",
@@ -26,10 +20,39 @@ def main() -> None:
     for value in variables_name:
         try:
             temp = get_variable(value)
-        except Exception as cur_error:
+        except KeyError as cur_error:
             print(f'Error: {cur_error}')
-            return
-        if not temp:
-            return
+            return None
         variables[value] = temp
-    
+    return variables
+
+
+def main() -> None:
+    print()
+    print('ORACLE STATUS: Reading the Matrix...')
+    print()
+    env_variables = get_env()
+    if env_variables is not None:
+        print('Configuration loaded:')
+        print(f"Mode: {env_variables['MATRIX_MODE']}")
+        print('Database: Connected to local instance')
+        print('API Access: Authenticated')
+        print(f"Log Level: {env_variables['LOG_LEVEL']}")
+        print('Zion network: Online')
+        print()
+    print('Environment security check:')
+    print('[OK] No hardcoded secrets detected')
+    if env_variables is None:
+        print('[KO] .env file not properly configured or missing')
+        return
+    print('[OK] .env file properly configured')
+    print('[OK] Production overrides available')
+    print()
+    print('The Oracle sees all configurations.')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as cur_error:
+        print(f'Error: {cur_error}')
